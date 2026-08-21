29765b1974eaabe6e94e5c7e884e02113a5acd53
Keoni Taimanglo <taimanglooni@gmail.com>
2026-08-21 14:49:52 -0700

    Pseudocode made for checkWinner method in GameControl

--- odin-projects/tic-tac-toe/script.js
+++ odin-projects/tic-tac-toe/script.js
@@ -9,7 +9,7 @@ function Player(name, marker) {
 }
 
 function Gameboard() {
-  // [x][y]
+  //  [x][y]
   //    x - row position
   //    y - column position
   let board = [
@@ -18,6 +18,13 @@ function Gameboard() {
     ["", "", ""],
   ];
 
+  //  rows and cols are used to grab/set board spots
+  //  arguments for getBoardPos/setBoardPos will be as such
+  //    'TOP', 'LEFT'
+  //    'TOP', 'MIDDLE',
+  //    'TOP', 'RIGHT',
+  //    ...
+  //    'BOTTOM', 'RIGHT'
   const rows = {
     TOP: 0,
     MIDDLE: 1,
@@ -56,26 +63,46 @@ function Gameboard() {
   };
 
   const resetBoard = () => {
-    for (let i = 0; i < board.length; i++) {
-      for (let j = 0; j < board[i].length; j++) {
-        board[i][j] = "";
+    for (const propRow in rows) {
+      for (const propCol in cols) {
+        board[rows[propRow]][cols[propCol]] = "";
       }
     }
   };
 
-  return { getBoardPos, setBoardPos, displayBoard, resetBoard };
+  return { getBoardPos, setBoardPos, displayBoard, resetBoard, rows, cols };
 }
 
 const GameControl = (() => {
-  let { getBoardPos, setBoardPos, displayBoard, resetBoard } = Gameboard();
+  let ttt = Gameboard();
 
-  const checkWinner = () => {};
+  const checkWinner = (rowPos, colPos) => {
+    let winFound = false;
+    const move = ttt.getBoardPos(rowPos, colPos);
+
+    //  check if move is empty and if empty, return winFound (false)
+    if (Object.getPrototypeOf(move) === String.prototype) {
+      return winFound;
+    }
+
+    //  wherever a move may be made, the row and col of that spot will be checked for a win
+    //  depending on the spot both or only one diagonal may be checked
+    //    spot is the center - check both diagonals
+    //    spot is a corner
+    //      spot is a top-left or bottom-right corner
+    //      spot is a top-right or bottom-left corner
+  };
 
   const resetGame = () => {
     resetBoard();
   };
 
-  return { setBoardPos, displayBoard, checkWinner, resetGame };
+  return {
+    setBoardPos: ttt.setBoardPos,
+    displayBoard: ttt.displayBoard,
+    checkWinner,
+    resetGame,
+  };
 })();
 
 // Test
