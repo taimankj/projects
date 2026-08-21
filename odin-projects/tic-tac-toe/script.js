640e579295507433f33d6305b28245102c7c0d8d
Keoni Taimanglo <taimanglooni@gmail.com>
2026-08-20 17:39:59 -0700

    Implement getBoardPos method

--- odin-projects/tic-tac-toe/script.js
+++ odin-projects/tic-tac-toe/script.js
@@ -17,7 +17,21 @@ function Gameboard() {
     [null, null, null],
   ];
 
-  const getBoardPos = (pos) => {};
+  const getBoardPos = (rowPos, colPos) => {
+    const rows = {
+      TOP: 0,
+      MIDDLE: 1,
+      BOTTOM: 2,
+    };
+
+    const cols = {
+      LEFT: 0,
+      MIDDLE: 1,
+      RIGHT: 2,
+    };
+
+    return board[rows[rowPos]][cols[colPos]];
+  };
 
   const setBoardPos = (rowPos, colPos, marker) => {
     const rows = {
@@ -37,9 +51,7 @@ function Gameboard() {
 
   const displayBoard = () => {
     console.log(`${board[0][0]} | ${board[0][1]} | ${board[0][2]}`);
-    console.log(`----------`);
     console.log(`${board[1][0]} | ${board[1][1]} | ${board[1][2]}`);
-    console.log(`----------`);
     console.log(`${board[2][0]} | ${board[2][1]} | ${board[2][2]}`);
   };
 
