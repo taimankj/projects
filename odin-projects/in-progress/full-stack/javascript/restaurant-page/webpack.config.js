ebae92280ed613effaf63723669fc6268ac68c53
Keoni Taimanglo <taimanglooni@gmail.com>
2026-09-10 11:26:38 -0700

    Create test content in template html and add styling
    installed css-loader and style-loader
    imported src/styles.css into src/index.js
    added styling for home page

--- odin-projects/in-progress/full-stack/javascript/restaurant-page/webpack.config.js
+++ odin-projects/in-progress/full-stack/javascript/restaurant-page/webpack.config.js
@@ -18,4 +18,12 @@ export default {
       template: "./src/template.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
